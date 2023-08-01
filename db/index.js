4228e5ce98e745227f51a2bd0178a14bb5335ea6
Bala212 <balajivaste212@gmail.com>
2023-08-02 01:52:40 +0530

    node version added

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 //connect database
+// const url = "mongodb://localhost:27017/movieApp"
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         console.log('Database is connected !');
@@ -8,4 +9,3 @@ mongoose.connect(process.env.MONGO_URL)
     .catch((err) => {
         console.log('Database connection failed : ', err);
     });
-
